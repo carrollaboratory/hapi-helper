e2ec94d6e859c87a0060356b3c4091ef67fa3790
Eric Torstenson <eric.s.torstenson@vumc.org>
2026-02-27 16:44:36 -0600

    Support for ImplementationGuide/ endpoint returns

--- scripts/load_ig.sh
+++ scripts/load_ig.sh
@@ -15,14 +15,23 @@ FHIR_SERVER=${1:-"http://localhost:8080/fhir"}
 # that option: i.e.
 #            curl -L --insecure -o /tmp/ig.tgz $IG
 if [[ "$IG" =~ ^[a-zA-Z]+://  && "$IG" == *.tgz  ]]; then
+    echo "***********************************"
+    echo "Downloading IG from $IG into /tmp"
+    echo -e "***********************************\n"
     curl -L -o /tmp/ig.tgz $IG
 elif [[ -e "$IG" && "$IG" == *.tgz ]]; then
+    echo "***********************************"
+    echo "Copying $IG to /tmp"
+    echo -e "***********************************\n"
     cp $IG /tmp/ig.tgz
 else
+    echo "***********************************"
     echo "IG URL must be a tgz file or a URL that points to a tgz file."
+    echo -e "***********************************\n"
     exit 1
 fi
 
+echo "POSTing the IG to the FHIR server, $FHIR_SERVER"
 BASE64_CONTENT=$(base64 -w 0 /tmp/ig.tgz)
 curl -s -X POST "$FHIR_SERVER/ImplementationGuide/\$install" \
   -H "Content-Type: application/json" \
@@ -37,3 +46,34 @@ curl -s -X POST "$FHIR_SERVER/ImplementationGuide/\$install" \
   ]
 }
 EOF
+echo ""
+# Extract the actual IG
+echo "***********************************"
+echo "Extracting the Implementation Guide JSON to /tmp"
+echo -e "***********************************\n"
+
+tar -xOzf /tmp/ig.tgz --wildcards --no-anchored 'ImplementationGuide*.json' > /tmp/ImplementationGuide.json
+ls -l /tmp/ImplementationGuide.json
+# I'm getting some errors about mcode's dependencies not matching the current
+# FHIR naming, so, I'm removing it as a depency in the JSON for now.
+echo "***********************************"
+echo "Removing some dependency notes to enable loading without turning off \
+strict validation in our test servers. This probably isn't appropriate for \
+a production server. However, those probably won't have stringent validation \
+activated. "
+echo -e "***********************************\n"
+
+jq 'del(.dependsOn, .extension, .definition)' /tmp/ImplementationGuide.json > /tmp/ImplementationGuide_clean.json
+
+ls -l /tmp/ImplementationGuide_clean.json
+
+IG_ID=$(jq -r '.id' /tmp/ImplementationGuide_clean.json)
+
+echo "***********************************"
+echo "PUTting to $FHIR_SERVER/ImplementationGuide/$IG_ID"
+echo -e "***********************************\n"
+
+# And POST it to the FHIR server
+curl -X PUT "$FHIR_SERVER/ImplementationGuide/$IG_ID" \
+     -H "Content-Type: application/fhir+json" \
+     -d @/tmp/ImplementationGuide_clean.json
